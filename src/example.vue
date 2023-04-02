202c201067c47954a203bfcbc2a47a33a787ca30
sklbz <sokolbozanic@gmail.com>
2023-04-02 10:53:48 +0200

    added a reactive click button

---
+++ src/example.vue
@@ -0,0 +1,22 @@
+<template>
+	<section>
+		<p v-if="likes>0">
+			THIS HAS {{ likes }} 👍
+		</p>
+		<p v-else>
+			nobody likes this
+		</p>
+
+		<button v-on:click="likes++">Click Me</button>
+	</section>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				likes: 0,
+			}
+		}
+	}
+</script>
